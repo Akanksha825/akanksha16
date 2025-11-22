c2ffe68cbabb053e62a99c232136f62e77154b0d
snehal <snehalpatil042001@gmail.com>
2025-11-22 15:30:12 +0530

    2expt.py

---
+++ 2expt.py
@@ -0,0 +1,8 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number"))
+add=num1+num2
+diff=num1-num2
+prod=num1*num2
+div=num1/num2
+rem=num1%num2
+print("\nThe addition of ",num1,"and",num2,"is:",add)
